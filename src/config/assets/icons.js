1f02b640863ba6f141534226da8c56ff2c1732d9
valb-mig <valb.mig@gmail.com>
2024-01-08 23:13:49 -0300

    :recycle:(Refatoração) Reconfigurando códigos de cálculo

--- src/config/assets/icons.js
+++ src/config/assets/icons.js
@@ -1,11 +1,18 @@
 import { 
     FaSearch,
-    FaMapPin
+    FaMapPin,
+    FaRoad
 } from "react-icons/fa";
+
+import { 
+    MdCo2
+} from "react-icons/md";
 
 const icons = {
     Search: FaSearch,
-    Pin: FaMapPin
+    Pin:    FaMapPin,
+    Co2:    MdCo2,
+    Road:   FaRoad
 }
 
 export default icons;
--- src/config/helpers/CalculoDistancia.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import haversine from 'haversine-distance';
-
-const CalculoDistancia = ({ pontoA, pontoB }) => {
-  const distanciaEmKm = haversine(pontoA, pontoB) / 1000; // Convertendo de metros para quilômetros
-  return <p>A distância entre os pontos {pontoA.nome} e {pontoB.nome} é de aproximadamente {distanciaEmKm.toFixed(2)} km.</p>;
-};
-
-export default CalculoDistancia;
